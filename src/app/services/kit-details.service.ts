e1debd30af0d0e809d0b761b81e9dfd0bec98fcf
Lucas Duarte <95879954+lucasduarte2@users.noreply.github.com>
2025-01-03 13:06:20 +0000

    kit--details

--- src/app/services/kit-details.service.ts
+++ src/app/services/kit-details.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class KitDetailsService {
-  private apiUrl = 'http://127.0.0.1:8000/api/kit'; // URL da sua API (ajuste conforme necessário)
+  private apiUrl = 'cultivua-laravel-production.up.railway.app/api/kit'; // URL da sua API (ajuste conforme necessário)
 
   constructor(private http: HttpClient) {}
 
